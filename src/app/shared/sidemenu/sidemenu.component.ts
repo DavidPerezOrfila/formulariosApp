92c47d57968647854954368ddf5fd07118784651
David Pérez <knightbcn@gmail.com>
2021-08-23 20:08:13 +0200

    template switches

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -8,7 +8,7 @@ interface MenuItem {
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styles: [
+  styles: [` li {cursor: pointer}`
   ]
 })
 export class SidemenuComponent {
--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,6 +1,17 @@
 import { Component, ViewChild, } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -9,6 +20,14 @@ import { NgForm } from '@angular/forms';
 })
 export class DinamicosComponent {
 
+  nuevoJuego: string = '';
+
+  persona: Persona = {
+    nombre: 'David',
+    favoritos: [{ id: 1, nombre: 'Metal Gear' }, { id: 2, nombre: 'Super Mario World' }]
+  }
+
+
   @ViewChild('miFormulario') miFormulario!: NgForm
 
   nombreValido(): boolean {
@@ -16,8 +35,20 @@ export class DinamicosComponent {
       && this.miFormulario?.controls.nombre?.touched
   }
 
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push({ ...nuevoFavorito })
+    this.nuevoJuego = ''
+  }
   guardar() {
     console.log('Formulario posteado');
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
 
