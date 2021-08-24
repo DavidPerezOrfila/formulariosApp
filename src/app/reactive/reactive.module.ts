6c44efb98e39196d1bff417a4a3317f639b6cfc7
David Pérez <knightbcn@gmail.com>
2021-08-24 20:12:51 +0200

    reactivos html

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { ReactiveRoutingModule } from './reactive-routing.module';
+// Componentes
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { ReactiveRoutingModule } from './reactive-routing.module';
 import { SwitchesComponent } from './switches/switches.component';
 
+// Módulos
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { SwitchesComponent } from './switches/switches.component';
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveFormsModule,
+    ReactiveRoutingModule,
   ]
 })
 export class ReactiveModule { }
