7cd5225d8fdaa41839dac317561ba125480432e1
David Pérez <knightbcn@gmail.com>
2021-09-02 19:23:42 +0200

    validator service

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -12,7 +13,7 @@ import { RegistroComponent } from './pages/registro/registro.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule, ReactiveFormsModule
   ]
 })
 export class AuthModule { }
